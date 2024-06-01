5d30c2d377fbf8379de5fa7027f086a1b1e7d5b9
TornadoJames <39809213+TornadoJames@users.noreply.github.com>
2024-06-01 11:18:32 -0500

    Update sensor.py

--- components/oxygens/sensor.py
+++ components/oxygens/sensor.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
 from esphome.const import (
-    ICON_PERCENT,
+    ICON_WIFI,
     UNIT_PERCENT,
     STATE_CLASS_MEASUREMENT,
 )
