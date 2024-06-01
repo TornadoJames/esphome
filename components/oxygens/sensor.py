0624101b3c9b7614f2767826fbd7c94c2b0d0044
TornadoJames <39809213+TornadoJames@users.noreply.github.com>
2024-05-31 20:45:33 -0500

    Update sensor.py

--- components/oxygens/sensor.py
+++ components/oxygens/sensor.py
@@ -2,25 +2,25 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
 from esphome.const import (
-    ICON_CHEMICAL_WEAPON,
-    UNIT_PARTS_PER_BILLION,
+    ICON_PERCENT,
+    UNIT_PERCENT,
     STATE_CLASS_MEASUREMENT,
 )
 
-CODEOWNERS = ["@notjj"]
+CODEOWNERS = ["@tornadojames"]
 DEPENDENCIES = ["i2c"]
 
-sen0321_sensor_ns = cg.esphome_ns.namespace("sen0321_sensor")
-Sen0321Sensor = sen0321_sensor_ns.class_(
-    "Sen0321Sensor", cg.PollingComponent, i2c.I2CDevice
+sen0321_sensor_ns = cg.esphome_ns.namespace("sen0322_sensor")
+Sen0321Sensor = sen0322_sensor_ns.class_(
+    "Sen0322Sensor", cg.PollingComponent, i2c.I2CDevice
 )
 
 CONFIG_SCHEMA = (
     sensor.sensor_schema(
-        Sen0321Sensor,
-        unit_of_measurement=UNIT_PARTS_PER_BILLION,
-        icon=ICON_CHEMICAL_WEAPON,
-        accuracy_decimals=1,
+        Sen0322Sensor,
+        unit_of_measurement=UNIT_PERCENT,
+        icon=ICON_PERCENT,
+        accuracy_decimals=2,
         state_class=STATE_CLASS_MEASUREMENT,
     )
     .extend(cv.polling_component_schema("60s"))
